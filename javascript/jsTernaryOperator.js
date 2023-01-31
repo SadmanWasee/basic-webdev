96574085b652e2103be36095a07f2f2f82d42e67
Sadman Wasee <sadman.wasee125@gmail.com>
2023-01-31 12:37:31 +0600

    Added codes related to javascript ternary operator

---
+++ javascript/jsTernaryOperator.js
@@ -0,0 +1,6 @@
+let number = Number(prompt("Enter a number")) ;
+
+// number > 0 ? console.log("Positive") : console.log("Negative")
+
+number > 0 ? console.log("Positive"): number < 0? console.log("Negative") : console.log("Zero");
+
