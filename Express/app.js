ff8c41916e6a33a26b6c91121135bf24acf78f99
Sadman Wasee <sadman.wasee125@gmail.com>
2023-04-19 17:03:11 +0600

    Made some changes to first express app codes

--- Express/app.js
+++ Express/app.js
@@ -3,14 +3,29 @@ const express = require("express");
 //creating an express app
 const app = express();
 
-//port 80 is used by default
+//We want to run our app on port 80 as it is used by default to run express app
 const port = 80;
 
-//Whenever client gets / implement the call back function
+//Handle the get request at "/" end point and implement the call back function
 app.get("/",(req,response)=>{
-    response.send("This is my home page first app")
+    response.send("This is my home page of my first app");
 });
 
+//Handle the get request at "/about" end point and implement the call back function
+app.get("/about",(req,response)=>{
+    response.send("This is my about page of my first app");
+});
+
+//Handle post request
+app.post("/about",(req,response)=>{
+    response.send("This is my about page of my first app");
+});
+
+//sending the status code
+app.get("/this",(req,response)=>{
+    response.status(404).send("This is page is not found");
+});
+//out app will listen for action from the port and seve accordingly 
 app.listen(port, ()=>{
-    console.log(`The application started successfully on ${port}`)
+    console.log(`The application started successfully on ${port}`);
 }); 
