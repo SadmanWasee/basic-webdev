d31c68f31ad3b7942a029a6a24a63041bc7c7bf6
Sadman Wasee <sadman.wasee125@gmail.com>
2023-04-21 11:50:48 +0600

    Added code and comments related to pug

--- Express/app.js
+++ Express/app.js
@@ -1,31 +1,24 @@
-//importing express module
-const express = require("express");
-//creating an express app
-const app = express();
 
-//We want to run our app on port 80 as it is used by default to run express app
-const port = 80;
+const express = require("express");  //importing express module
+const path = require('path');  //setting path
+const app = express();  //creating an express app
+const port = 80;  //We want to run our app on port 80 as it is used by default to run express app
 
-//Handle the get request at "/" end point and implement the call back function
-app.get("/",(req,response)=>{
-    response.send("This is my home page of my first app");
+//Express specific stuff
+app.use('/static', express.static('static'))  // For serving static files '/static' - URL name and 'static' - folder name
+
+//Pug specific stuff
+app.set('view engine', 'pug');  //set the template engine as pug 
+app.set('views', path.join(__dirname,'views'))  //set the views directory
+
+//Endpoints
+app.get('/', (req,res)=>{
+    const con = "This is a beginner tutorial on pug"
+    const params ={'title': 'This is pug tutorial', 'content':con}
+    res.status(200).render('index.pug',params);
 });
 
-//Handle the get request at "/about" end point and implement the call back function
-app.get("/about",(req,response)=>{
-    response.send("This is my about page of my first app");
-});
-
-//Handle post request
-app.post("/about",(req,response)=>{
-    response.send("This is my about page of my first app");
-});
-
-//sending the status code
-app.get("/this",(req,response)=>{
-    response.status(404).send("This is page is not found");
-});
-//out app will listen for action from the port and seve accordingly 
+//Start server
 app.listen(port, ()=>{
-    console.log(`The application started successfully on ${port}`);
+    console.log(`The application started successfully on ${port}`); 
 }); 
